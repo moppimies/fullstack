8b8444457fbb69a90d4d0e495f1c8a8e5fa34d2f
vijumato <viljamitohonen@gmail.com>
2023-07-03 20:12:49 +0300

    3.11

--- osa3/puhelinluetteloBackend/index.js
+++ osa3/puhelinluetteloBackend/index.js
@@ -6,6 +6,7 @@ const morgan = require('morgan')
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(cors())
+app.use(express.static('build'))
 
 morgan.token('body', (req, res) => JSON.stringify({name:req.body.name,number:req.body.number}))
 
